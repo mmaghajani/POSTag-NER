2a2ebc310eb8071c31148f13241997cf976bf623
mmaghajani <mma137421@gmail.com>
2018-05-05 19:05:21 +0430

    51 percent accuray reached!

--- POSTagger.py
+++ POSTagger.py
@@ -1,5 +1,5 @@
 from nltk.tag import hmm
-
+import pprint
 train = list()
 sequence = list()
 cnt = 0
@@ -7,14 +7,11 @@ with open("data/POStrutf.txt", 'r') as file:
     for line in file.readlines():
         words = line.split("\t")
         sequence.append((words[0].strip(), words[2].strip()))
-        if cnt is 20:
+        if words[0].strip() == ".":
             train.append(sequence)
             cnt = 0
             sequence = list()
         cnt += 1
-    train.append(sequence)
-    cnt = 0
-    sequence = list()
     file.close()
 
 test = dict()
@@ -27,24 +24,13 @@ with open("data/POSteutf.txt", 'r') as file:
 test_words = list(map(lambda x: x[0], test.items()))
 test_label = list(map(lambda x: x[1], test.items()))
 
-print("train data", test_words)
-
-
-# Setup a trainer with default(None) values
-# And train with the data
-trainer = hmm.HiddenMarkovModelTrainer()
-tagger = trainer.train_supervised(train)
+tagger = hmm.HiddenMarkovModelTagger.train(train)
 
 cnt = 0
 predict = tagger.tag(test_words)
 predict_words = list(map(lambda x:x[0], predict))
-print(predict_words)
-for i in range(0,len(predict_words)):
-    if predict_words[i] not in test_words:
-        print(predict_words[i])
 
 for i in range(0,len(predict)):
-    print(predict[i][0], predict[i][1], test[predict[i][0]])
     if predict[i][1] == test[predict[i][0]]:
         cnt += 1
-print(cnt/len(predict))
+print("Accuracy : ", cnt/len(predict))
